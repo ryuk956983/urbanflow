30de6e3f8108085abdd81456a8752865eb66a759
ryuk956983 <106580255+ryuk956983@users.noreply.github.com>
2026-04-22 06:04:29 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://urbanflow-e6yj.onrender.com",
     credentials:true
 }))
 
